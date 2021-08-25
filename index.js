c7bbb5b5f6e91e5b5faae226def560bd828ff99e
alex Mendoza <tiburon9386@gmail.com>
2021-08-25 18:33:17 -0500

    ya podemos agregar productos desde un formulario

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const path = require('path');
 // habilitar path
 // -----------------------------------
 // requerimos nuestro modelo
-const Product =require('./models/product');
+const Product = require('./models/product');
 
 // --------------------------------------
 // importamos mongoose
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/farmStand', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('conexion abierta')
     })
@@ -25,6 +25,8 @@ app.set('views', path.join(__dirname, 'views'));
 // esto es para correr el archide desde cualquier lugar
 app.set('view engine', 'ejs');
 // esto es para hacer ejs nuestro motor
+app.use(express.urlencoded({ extended: true }))
+// con esto le pedimos a expres que nos regrese un resultado del post
 
 // -----------------------------------------
 // esto espera a una respuesta del servidor en este caso mongod
@@ -35,16 +37,29 @@ app.set('view engine', 'ejs');
 // })
 // -------------------------------------------
 // ahora respondemos con un template de ejs
-app.get('/products',async (req, res) => {
+app.get('/products', async (req, res) => {
     const products = await Product.find({})
     console.log(products)
-    res.render('products/index', {products})
+    res.render('products/index', { products })
 })
 // ----------------------------------------------
 // app.get('/dog', (req, res) => {
 //     res.send('woof')
 // })
 // esto imprime woof en: localhost:3000/dog, corriendo nodemon index.js
+// -----------------------------------------
+// es hora de hacer nuevos productos desde un formulario
+app.get('/products/new', (req, res) => {
+    res.render('products/new')
+})
+
+app.post('/products', async (req, res) => {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    console.log(newProduct);
+    res.send('procesando los productos')
+})
+
 // ----------------------------------------------
 // detalles de productos
 // app.get('/products/:id', async(req, res) => {
@@ -55,11 +70,11 @@ app.get('/products',async (req, res) => {
 // })
 // y listo mostramos el mensaje de details page simulando mostrar un template de ejs 
 //  ahora viene template de ejs
-app.get('/products/:id', async(req, res) => {
-    const {id} = req.params;
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
     const product = await Product.findById(id);
     console.log(product);
-    res.render('products/show', {product})
+    res.render('products/show', { product })
 })
 
 
