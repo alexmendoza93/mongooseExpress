8dea66e45efaaaf31173e9a602750ca9ae8dfbfe
alex Mendoza <tiburon9386@gmail.com>
2021-08-25 12:11:42 -0500

    subimos un monton de productos al mismo tiempo a mongo

--- semillas.js
+++ semillas.js
@@ -14,15 +14,47 @@ mongoose.connect('mongodb://localhost:27017/farmStand', {useNewUrlParser: true, 
     })
 // nos conectamos con mongoose
 
-const p = new Product({
-    name: 'Grapefruit',
-    price: 1.89,
-    category: 'fruit'
-})
-// esto hace un nuevo producto
-p.save().then(p => {
-    console.log(p)
-})
-.catch(e => {
-    console.log(e)
-})
+// const p = new Product({
+//     name: 'Grapefruit',
+//     price: 1.89,
+//     category: 'fruit'
+// })
+// // esto hace un nuevo producto
+// p.save().then(p => {
+//     console.log(p)
+// })
+// .catch(e => {
+//     console.log(e)
+// })
+const seedProducts = [
+    {
+        name: 'zanahoria',
+        price: 10,
+        category: 'vegetable'
+    },
+    {
+        name: 'melon',
+        price: 13,
+        category: 'fruit'
+    },
+    {
+        name: 'sandia',
+        price: 9,
+        category: 'fruit'
+    },
+    {
+        name: 'cebolla',
+        price: 14,
+        category: 'vegetable'
+    }
+    
+]
+
+Product.insertMany(seedProducts)
+// cuando son varios productos no hace falta guardarlo, eso se hace en automatico, entonces:
+    .then(res => {
+        console.log(res)
+    })
+    .catch(e => {
+        console.log(e)
+    })
