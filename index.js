59d7e8385805d4d3dce60c35c165ca5d48a408b1
alex Mendoza <tiburon9386@gmail.com>
2021-08-26 07:37:32 -0500

    agregamos seleccion automatica de categoria en plantilla edit y agregamos una nueva categoria

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true,
         console.log(err)
     })
 // ------------------------------------------
+const categories = ['fruit', 'vegetable', 'dairy', 'fungi'];
+
 const methodOverride = require('method-override')
 
 app.use(methodOverride('_method'));
@@ -53,7 +55,7 @@ app.get('/products', async (req, res) => {
 // -----------------------------------------
 // es hora de hacer nuevos productos desde un formulario
 app.get('/products/new', (req, res) => {
-    res.render('products/new')
+    res.render('products/new', { categories })
 })
 
 app.post('/products', async (req, res) => {
--- models/product.js
+++ models/product.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const productSchema = new mongoose.Schema({
     name: {
         type: String,
-         required: true
+        required: true
     },
     price: {
         type: Number,
@@ -15,9 +15,9 @@ const productSchema = new mongoose.Schema({
     category: {
         type: String,
         lowercase: true,
-        enum: ['fruit', 'vegetable', 'dairy']
+        enum: ['fruit', 'vegetable', 'dairy', 'fungi']
     }
-    
+
 })
 
 const Product = mongoose.model('Product', productSchema);
